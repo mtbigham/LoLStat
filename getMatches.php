aa52b344ad425ef413d95ba762205c0832949633
mastergates <mastergates@users.noreply.github.com>
2015-03-14 07:31:54 -0400

    Update getMatches.php

--- getMatches.php
+++ getMatches.php
@@ -1,53 +1,66 @@
 <?php
-/*
- For each match in matchIDs, aggregate metrics by minute
- 
- HAVE: matchID, summonerID, champID
- SEARCH: participants->champID
- RETRIEVE: participants->participantID
- 
- HAVE: matchID, summonerID, champID, participantID
- SEARCH: timeline->foreach(frames)->participantFrames[participantID]->gold, lvl, xp, etc
- */
- 
- error_reporting(E_ALL);
- ini_set('display_errors', '1');
- 
-if(isset($_GET['summonerID']) && isset($_GET['champID'])) {
-    $matchIDs = [137226490, 137269904];
 
-    $stats = array();
+$championID = 67;
 
-    foreach ($matchIDs as $match) {
-        $myStats = array(
-            "currentGold" => [],
-            "totalGold" => [],
-            "level" => [],
-            "xp" => [],
-            "minionsKilled" => [],
-            "jungleMinionsKilled" => []
-        );
+$matchIDS = array(
+	"1681263045",
+	"1684827093",
+	"1684847240",
+	"1686599293"
+);
 
-        $game = json_decode(file_get_contents("https://lan.api.pvp.net/api/lol/lan/v2.2/match/{$match}?includeTimeline=true&api_key=5d8531ab-35eb-4502-8032-2dcd11a82683"));
+$csCounter = [];
 
-        $partID = '';
+$cs = [];
+$level = [];
+$gold = [];
 
-        foreach ($game->participants as $participant => $vals) {
-            if ($game->participants[$participant]->championId == $_GET['champID']) {
-                $partID = $game->participants[$participant]->participantId;
-            }
-        }
+foreach($matchIDS as $matchID){
+	
+	$api = "https://na.api.pvp.net/api/lol/na/v2.2/match/" . $matchID . "?includeTimeline=true&api_key=dc2ba6c8-4971-4a8a-87a9-d3c59db6f26b";
+	$api = json_decode(file_get_contents($api),true);
 
-        foreach ($game->timeline->frames as $frame => $frameArr) {
-            foreach ($frameArr->participantFrames->$partID as $valName => $val) {
-                if (array_key_exists($valName, $myStats)) {
-                    array_push($myStats[$valName], $val);
-                }
-            }
-        }
+	$participantID = '';
 
-        array_push($stats, $myStats);
-    }
-    print_r($stats);
+	foreach($api['participants'] as $player){
+		if($player['championId'] == $championID){
+			$participantID = $player['participantId'];
+			break;
+		}
+	}
+
+	//echo $participantID;
+
+	$count = 0;
+	foreach($api['timeline']['frames'] as $minute){
+		//print_r($minute['participantFrames'][$participantID]);
+		$handle = $minute['participantFrames'][$participantID];
+		
+		$cs[$count] += $handle['minionsKilled'];
+			$csCounter[$count] += 1;
+		$level[$count] += $handle['level'];
+		$gold[$count] += $handle['totalGold'];
+
+		$count++;
+	}
+
+	$herp = array("herp","derp");
+	//echo ($herp[2] == NULL ? "nothing here" : $herp[2]);
 }
+
+$max = count($csCounter);
+echo $max;
+for($i = 0; $i<$max; $i++){
+	$cs[$i] = $cs[$i] / $csCounter[$i];
+	$level[$i] = $level[$i] / $csCounter[$i];
+	$gold[$i] = $gold[$i] / $csCounter[$i];
+}
+
+echo "cs";
+print_r($cs);
+echo "level";
+print_r($level);
+echo "gold";
+print_r($gold);
+print_r($csCounter);
 ?>
